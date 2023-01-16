f60be2159089fff1c06f509c3085b5c657add8f3
mijumaruccino <77814710+mijumaruccino@users.noreply.github.com>
2023-01-15 22:25:40 -0500

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63c4b72e5f4517002424ecd5",
+				"63c4b72e5f4517002424ecd8"
+			]
+		},
+		"63c4b72e5f4517002424ecd5": {
+			"id": "63c4b72e5f4517002424ecd5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63c4b72e5f4517002424ecd8": {
+			"id": "63c4b72e5f4517002424ecd8",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
